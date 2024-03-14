15ca204a4f017d04e7c2269b3e4516dfc34643d5
Cralcas <carlosbergq@gmail.com>
2024-03-14 13:38:20 +0100

    Retrieve items from db and render them ✨

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
@@ -19,13 +20,27 @@ const inputFIeldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 const shoppingListEl = document.getElementById("shopping-list");
 
-addButtonEl.addEventListener("click", () => {
+addButtonEl.addEventListener("click", function () {
   let inputValue = inputFIeldEl.value;
 
   push(shoppingListInDB, inputValue);
 
-  inputFIeldEl.value = "";
-
-  shoppingListEl.innerHTML += `<li>${inputValue}</li>`;
-  console.log(inputValue);
+  clearInputField();
+  addItemToShoppingList(inputValue);
 });
+
+onValue(shoppingListInDB, function (snapshot) {
+  let itemsArray = Object.values(snapshot.val());
+
+  for (let i = 0; i < itemsArray.length; i++) {
+    addItemToShoppingList(itemsArray[i]);
+  }
+});
+
+function clearInputField() {
+  inputFIeldEl.value = "";
+}
+
+function addItemToShoppingList(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
