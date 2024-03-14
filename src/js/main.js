e6b7b6729f0511746b67154784a105833f3e9a4d
Cralcas <carlosbergq@gmail.com>
2024-03-14 10:15:24 +0100

    Setting up Firebase ✨

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,27 @@
 import "../css/style.css";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://realtime-database-c718c-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFIeldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+
+addButtonEl.addEventListener("click", () => {
+  let inputValue = inputFIeldEl.value;
+
+  push(shoppingListInDB, inputValue);
+
+  console.log(inputValue);
+});
