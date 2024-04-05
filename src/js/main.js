1bb9e2de46b9c64a1be45953df71184a8f878a7f
Cralcas <carlosbergq@gmail.com>
2024-04-05 07:47:59 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import "../css/style.css";
+import "/src/css/style.css";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
